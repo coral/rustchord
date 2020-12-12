7f1de0acb5e165eb84d02d3866e0b20aa63c2c3a
coral <jonas@jonasbengtson.se>
2020-12-12 00:03:46 -0800

    first working build (THANKS iaVh3z)

--- build.rs
+++ build.rs
@@ -1,3 +1,25 @@
+extern crate cc;
+
 fn main() {
+    cc::Build::new()
+        .shared_flag(true)
+        .files(&[
+            "colorchord/colorchord2/color.c",
+            "colorchord/colorchord2/notefinder.c",
+            "colorchord/colorchord2/dft.c",
+            "colorchord/colorchord2/filter.c", 
+            "colorchord/colorchord2/decompose.c",
+            "colorchord/colorchord2/parameters.c", 
+            "colorchord/colorchord2/hook.c", 
+            "colorchord/colorchord2/configs.c",
+            "colorchord/colorchord2/chash.c",
+            "colorchord/embeddedcommon/DFT32.c",
+        ])
+        .include("colorchord/colorchord2")
+        .include("colorchord/colorchord2/rawdraw")
+        .include("colorchord/embeddedcommon")
+        .include("./include")
+        .compile("colorchord");
+
     println!(r"cargo:rustc-link-search=."); 
 }
