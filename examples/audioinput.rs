9b3ce9dadd55d3663598908cbc62107250cd88fb
coral <jonas@jonasbengtson.se>
2020-12-23 22:31:13 -0800

    I HATE MY LIFE

--- examples/audioinput.rs
+++ examples/audioinput.rs
@@ -1,15 +1,16 @@
-use cpal;
 use anyhow::Error;
+use cpal;
 use cpal::traits::{DeviceTrait, HostTrait};
-use std::sync::mpsc::*;
-use std::slice;
 use rustchord::{self, cc_to_rgb};
+use std::slice;
+use std::sync::mpsc::*;
 
 use piston_window;
 
 use piston_window::*;
 
-#[link(name = "colorchord")] extern {} 
+#[link(name = "colorchord")]
+extern "C" {}
 
 struct Note {
     color: u32,
@@ -18,148 +19,156 @@ struct Note {
 
 fn main() {
     println!("Hello, world!");
-    
-    let mut window: PistonWindow =
-    WindowSettings::new("Hello Piston!", [1400, 480])
-    .exit_on_esc(true).build().unwrap();
+
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [1400, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
     window.next();
-    
     let (tx, rx) = channel();
     let mut r = ringbuffer::new(tx);
     let host = cpal::default_host();
-    
     let mut notefinder = rustchord::Notefinder::new(48000);
-    
     let device = host
-    .default_input_device()
-    .expect("Failed to get default input device");
-    
+        .default_input_device()
+        .expect("Failed to get default input device");
     println!("{:?}", device.name());
-    
     let config = &cpal::StreamConfig {
         channels: 1,
         buffer_size: cpal::BufferSize::Fixed(1024),
-        sample_rate: cpal::SampleRate(48_000)
+        sample_rate: cpal::SampleRate(48_000),
     };
-    
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
     };
-    
-    let stream = device.build_input_stream(config,
+
+    let stream = device.build_input_stream(
+        config,
         move |data, inp: &cpal::InputCallbackInfo| r.audio_callback(data, inp),
-        err_fn);
-        
-        while let Ok(v) = rx.recv() {
-            notefinder.run(&v);
-            let res = notefinder.get_folded();
+        err_fn,
+    );
 
-            let width = window.size().width / 24.;
-            let bottom = window.size().height / 2.;
-            
-            if let Some(event) = window.next() {
+    while let Ok(v) = rx.recv() {
+        //let bm: Vec<_> = v.into_iter().step_by(2).collect();
 
-                window.draw_2d(&event, |context, graphics, _device| {
-                    clear([1.0; 4], graphics);
+        notefinder.run(&v);
+        let notes = notefinder.get_notes();
+        let res = notefinder.get_folded();
 
-                    //Frequency bins
-                    for (i, n) in res.into_iter().enumerate() {
+        let width = window.size().width / 24.;
+        let notew = window.size().width / 12.;
+        let middle = window.size().height / 2.;
+        let bottom = window.size().height;
 
-                        let c = cc_to_rgb((i as f32 + 0.5) / 24., 1.0, 1.0);
+        if let Some(event) = window.next() {
+            window.draw_2d(&event, |context, graphics, _device| {
+                clear([1.0; 4], graphics);
 
-                        rectangle([c[0], c[1], c[2], 1.0],
-                            [width * i as f64, bottom, width, -(*n as f64 * 800.)],
-                            context.transform,
-                            graphics);
+                //Frequency bins
+                for (i, n) in res.into_iter().enumerate() {
+                    let c = cc_to_rgb((i as f32 + 0.5) / 24., 1.0, 1.0);
 
-                    }
-
-                });
-            }
-
-            // for n in 0..res.notepeaks {
-            //     if res.amplitudes[n as usize] < 0.0 {
-            //         continue;
-            //     }
-            //     //print!("{:?} ", res.positions[n as usize]/res.freqbins as f32);
-
-            // }
-
-            // unsafe {
-            //     rustchord::RunNoteFinder(notefinder, v.as_ptr(), 0, 1024)
-            // }
-            // unsafe {
-            //     //println!("{:?}", (*notefinder).freqbins);
-            //     //println!("{:?}", (*notefinder).freqbins * (*notefinder).octaves);
-            //     let notepeaks = (*notefinder).freqbins / 2;
-
-            //     let mut bins = Vec::new();
-                
-            //     for l in  0..notepeaks {
-            //         if *(*notefinder).note_amplitudes_out.offset(l as isize) < 0.0 {
-            //             continue;
-            //         }
-            //         let note = *(*notefinder).note_positions.offset(l as isize) / (*notefinder).freqbins as f32;
-            //         //print!("{:?} ", note);
-            //         // bins.push(Note{
-            //         //     amplitude: *(*notefinder).note_amplitudes_out.offset(l as isize),
-            //         //     color: rustchord::CCtoHEX(note, 1.0, 1.0),
-            //         // })
-            //     }
-                
-                // if let Some(event) = window.next() {
-                    
-                    
-                //     window.draw_2d(&event, |context, graphics, _device| {a
-                //         clear([1.0; 4], graphics);
-
-                //         for (i, n) in bins.iter().enumerate() {
-                //            // print!("{:x} ", b.color);
-                //             let (b, g, r) = ( (n.color >> 16)&0xff, (n.color >> 8)&0xff, n.color&0xff );
-                //             let (r, g, b) = (r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0); 
-
-                            
-
-                //          }
-                //         });
-                //     }
-                    
-                    
-                // }
-                
-        }
-        std::thread::sleep(std::time::Duration::from_secs(120));
-        drop(stream);
-            
-            
-        }
-        
-        
-        pub struct ringbuffer {
-            buffersize: usize,
-            soundhead: usize,
-            buffer: [f32; 8192],
-            nf: Sender<Vec<f32>>,
-        }
-        
-        impl ringbuffer {
-            fn new(nf: Sender<Vec<f32>>) -> ringbuffer {
-                return ringbuffer {
-                    buffersize: 8192,
-                    buffer: [0.0f32; 8192],
-                    soundhead: 0,
-                    nf,
+                    rectangle(
+                        [c[0], c[1], c[2], 1.0],
+                        [width * i as f64, bottom, width, -(*n as f64 * 800.)],
+                        context.transform,
+                        graphics,
+                    );
                 }
-            }
-            
-            fn audio_callback(&mut self, input: &[f32], denis: &cpal::InputCallbackInfo) {
-                self.buffer[self.soundhead .. self.soundhead + input.len()].copy_from_slice(input);
-                self.soundhead += input.len();
-                
-                if self.soundhead >= self.buffersize {
-                    self.soundhead = self.soundhead % self.buffersize;
+
+                for (i, n) in notes.into_iter().enumerate() {
+                    let c = cc_to_rgb((n.id as f32 + 0.5) / 24., 1.0, 1.0);
+
+                    rectangle(
+                        [c[0], c[1], c[2], 1.0],
+                        [
+                            notew * i as f64,
+                            middle,
+                            notew,
+                            -(n.amplitude_out as f64 * 200.),
+                        ],
+                        context.transform,
+                        graphics,
+                    )
                 }
-                
-                self.nf.send(input.to_vec());
-            }
+            });
         }
+
+        // for n in 0..res.notepeaks {
+        //     if res.amplitudes[n as usize] < 0.0 {
+        //         continue;
+        //     }
+        //     //print!("{:?} ", res.positions[n as usize]/res.freqbins as f32);
+
+        // }
+
+        // unsafe {
+        //     rustchord::RunNoteFinder(notefinder, v.as_ptr(), 0, 1024)
+        // }
+        // unsafe {
+        //     //println!("{:?}", (*notefinder).freqbins);
+        //     //println!("{:?}", (*notefinder).freqbins * (*notefinder).octaves);
+        //     let notepeaks = (*notefinder).freqbins / 2;
+
+        //     let mut bins = Vec::new();
+
+        //     for l in  0..notepeaks {
+        //         if *(*notefinder).note_amplitudes_out.offset(l as isize) < 0.0 {
+        //             continue;
+        //         }
+        //         let note = *(*notefinder).note_positions.offset(l as isize) / (*notefinder).freqbins as f32;
+        //         //print!("{:?} ", note);
+        //         // bins.push(Note{
+        //         //     amplitude: *(*notefinder).note_amplitudes_out.offset(l as isize),
+        //         //     color: rustchord::CCtoHEX(note, 1.0, 1.0),
+        //         // })
+        //     }
+
+        // if let Some(event) = window.next() {
+
+        //     window.draw_2d(&event, |context, graphics, _device| {a
+        //         clear([1.0; 4], graphics);
+
+        //         for (i, n) in bins.iter().enumerate() {
+        //            // print!("{:x} ", b.color);
+        //             let (b, g, r) = ( (n.color >> 16)&0xff, (n.color >> 8)&0xff, n.color&0xff );
+        //             let (r, g, b) = (r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0);
+
+        //          }
+        //         });
+        //     }
+
+        // }
+    }
+    std::thread::sleep(std::time::Duration::from_secs(120));
+    drop(stream);
+}
+
+pub struct ringbuffer {
+    buffersize: usize,
+    soundhead: usize,
+    buffer: [f32; 8192],
+    nf: Sender<Vec<f32>>,
+}
+
+impl ringbuffer {
+    fn new(nf: Sender<Vec<f32>>) -> ringbuffer {
+        return ringbuffer {
+            buffersize: 8192,
+            buffer: [0.0f32; 8192],
+            soundhead: 0,
+            nf,
+        };
+    }
+
+    fn audio_callback(&mut self, input: &[f32], denis: &cpal::InputCallbackInfo) {
+        self.buffer[self.soundhead..self.soundhead + input.len()].copy_from_slice(input);
+        self.soundhead += input.len();
+
+        if self.soundhead >= self.buffersize {
+            self.soundhead = self.soundhead % self.buffersize;
+        }
+
+        self.nf.send(input.to_vec());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,33 @@
 use std::slice;
 mod internal;
 
-use palette::{FromColor, IntoColor, Lch, Saturate, Shade, Srgb, Hsv, Hue, Pixel};
+use palette::{FromColor, Hsv, IntoColor, Pixel, Srgb};
 
 pub struct NotefinderResult<'a> {
     pub freqbins: i32,
     pub notepeaks: i32,
     pub positions: &'a [f32],
     pub amplitudes: &'a [f32],
-    pub folded: &'a [f32], 
-    pub dists: Vec<NoteDists>
-}
-pub struct NoteDists {
-    pub amp: f32,   //Amplitude of normal distribution
-    pub mean: f32,  //Mean of normal distribution
-    pub sigma: f32, //Sigma of normal distribution
-    pub taken: bool //Is distribution associated with any notes?
+    pub folded: &'a [f32],
+    pub dists: Vec<NoteDists>,
 }
 
+#[derive(Debug)]
+pub struct NoteDists {
+    pub amp: f32,    //Amplitude of normal distribution
+    pub mean: f32,   //Mean of normal distribution
+    pub sigma: f32,  //Sigma of normal distribution
+    pub taken: bool, //Is distribution associated with any notes?
+}
+
+#[derive(Debug)]
 pub struct Note {
+    pub active: bool,
+    pub id: f32,
     pub dist: NoteDists,
+    pub amplitude_out: f32,
+    pub amplitude_iir2: f32,
+    pub endured: i32,
 }
 
 pub struct Notefinder {
@@ -29,77 +37,99 @@ pub struct Notefinder {
 unsafe impl Send for Notefinder {}
 
 impl Notefinder {
-    
-    pub fn new(samplerate:i32) -> Notefinder {
-        return Notefinder{
-            nf: unsafe{internal::CreateNoteFinder(samplerate)},
-        }
+    pub fn new(samplerate: i32) -> Notefinder {
+        return Notefinder {
+            nf: unsafe { internal::CreateNoteFinder(samplerate) },
+        };
     }
-    
     pub fn run(&mut self, data: &[f32]) {
+        //dbg!(data);
         unsafe {
             internal::RunNoteFinder(self.nf, data.as_ptr(), 0, data.len() as i32);
         }
     }
-    
-    pub fn get_notes<'a> (&'a self) -> NotefinderResult<'a>  {
-        let freqbins = unsafe{(*self.nf).freqbins};
-        let notepeaks = freqbins / 2;
-        
-        return NotefinderResult{
-            freqbins,
-            notepeaks,
-            positions:  unsafe{ slice::from_raw_parts((*self.nf).note_positions, freqbins as usize)},
-            amplitudes: unsafe{ slice::from_raw_parts((*self.nf).note_amplitudes_out, notepeaks as usize)},
-            folded: unsafe{ slice::from_raw_parts((*self.nf).folded_bins, freqbins as usize)},
-            dists: unsafe{ (0..notepeaks).map(|i| { let raw = (*self.nf).dists.offset(i as isize);
-                NoteDists { 
-                    amp: (*raw).amp,
-                    mean: (*raw).mean,
-                    sigma: (*raw).sigma,
-                    taken: (*raw).taken != 0,
-                }
+
+    // pub fn get_notes_old<'a>(&'a self) -> NotefinderResult<'a> {
+    //     let freqbins = unsafe { (*self.nf).freqbins };
+    //     let notepeaks = freqbins / 2;
+
+    //     return NotefinderResult {
+    //         freqbins,
+    //         notepeaks,
+    //         positions: unsafe {
+    //             slice::from_raw_parts((*self.nf).note_positions, freqbins as usize)
+    //         },
+    //         amplitudes: unsafe {
+    //             slice::from_raw_parts((*self.nf).note_amplitudes_out, notepeaks as usize)
+    //         },
+    //         folded: unsafe { slice::from_raw_parts((*self.nf).folded_bins, freqbins as usize) },
+    //         dists: unsafe {
+    //             (0..notepeaks)
+    //                 .map(|i| {
+    //                     let raw = (*self.nf).dists.offset(i as isize);
+    //                     NoteDists {
+    //                         amp: (*raw).amp,
+    //                         mean: (*raw).mean,
+    //                         sigma: (*raw).sigma,
+    //                         taken: (*raw).taken != 0,
+    //                     }
+    //                 })
+    //                 .collect::<Vec<_>>()
+    //         },
+    //     };
+    // }
+
+    pub fn get_notes(&self) -> Vec<Note> {
+        let freqbins: f32 = unsafe { (*self.nf).freqbins } as f32;
+        let mut notes: Vec<Note> = Vec::new();
+        for i in 0..12 {
+            unsafe {
+                let dist = (*self.nf).dists.offset(i as isize);
+                notes.push(Note {
+                    active: *(*self.nf).note_amplitudes_out.offset(i as isize) > 0.,
+                    id: *(*self.nf).note_positions.offset(i as isize) / freqbins,
+                    dist: NoteDists {
+                        amp: (*dist).amp,
+                        mean: (*dist).mean,
+                        sigma: (*dist).sigma,
+                        taken: (*dist).taken != 0,
+                    },
+                    amplitude_out: { *(*self.nf).note_amplitudes_out.offset(i as isize) },
+                    amplitude_iir2: { *(*self.nf).note_amplitudes2.offset(i as isize) },
+                    endured: { *(*self.nf).enduring_note_id.offset(i as isize) },
+                })
             }
-        ).collect::<Vec<_>>()}
-        
+        }
+
+        notes
+    }
+
+    pub fn get_folded<'a>(&'a self) -> &'a [f32] {
+        return unsafe {
+            slice::from_raw_parts((*self.nf).folded_bins, (*self.nf).freqbins as usize)
+        };
     }
 }
 
-pub fn get_folded<'a> (&'a self) -> &'a [f32]{
-    return unsafe{ slice::from_raw_parts((*self.nf).folded_bins, (*self.nf).freqbins as usize)}; 
-}
+pub struct Color {}
 
-}
-
-pub struct Color {
-    
-}
-
-    pub fn cc_to_rgb(mut note: f32, saturation: f32, value: f32) -> [f32; 3] {
-        
-        let mut hue = 0.0;
-        note %= 1.0;
-        note *= 12.0;
-        if note < 4.0 
-        {
-            //Needs to be YELLOW->RED
-            hue = (4.0 - note) / 24.0;
-        }
-        else if note < 8.0 
-        {
-            //            [4]  [8]
-            //Needs to be RED->BLUE
-            hue = ( 4.0 - note ) / 12.0;
-        }
-        else
-        {
-            //             [8] [12]
-            //Needs to be BLUE->YELLOW
-            hue = ( 12.0 - note ) / 8.0 + 1.0/6.0;
-        }
-    
-        let c:Hsv = Hsv::new(hue*360., saturation, value).into_color(); 
-        Srgb::from_color(c).into_format().into_raw()
-        
+pub fn cc_to_rgb(mut note: f32, saturation: f32, value: f32) -> [f32; 3] {
+    let mut hue = 0.0;
+    note %= 1.0;
+    note *= 12.0;
+    if note < 4.0 {
+        //Needs to be YELLOW->RED
+        hue = (4.0 - note) / 24.0;
+    } else if note < 8.0 {
+        //            [4]  [8]
+        //Needs to be RED->BLUE
+        hue = (4.0 - note) / 12.0;
+    } else {
+        //             [8] [12]
+        //Needs to be BLUE->YELLOW
+        hue = (12.0 - note) / 8.0 + 1.0 / 6.0;
     }
-    
+
+    let c: Hsv = Hsv::new(hue * 360., saturation, value).into_color();
+    Srgb::from_color(c).into_format().into_raw()
+}
