a503a48f66284d032170301619240501b5372b83
coral <jonas@jonasbengtson.se>
2020-12-24 22:02:33 -0800

    threadedg

--- examples/audioinput.rs
+++ examples/audioinput.rs
@@ -2,69 +2,49 @@ use cpal;
 use cpal::traits::{DeviceTrait, HostTrait};
 use rustchord::{self, cc_to_rgb};
 use std::sync::mpsc::*;
+use std::thread;
 
 use piston_window;
 
 use piston_window::*;
 
-fn main() {
-    println!("Hello, world!");
+struct NoteResult {
+    notes: Vec<rustchord::Note>,
+    folded: Vec<f32>,
+}
 
+fn main() {
+    let (tx, rx) = channel::<NoteResult>();
+    thread::spawn(move || audioprocess(tx));
     let mut window: PistonWindow = WindowSettings::new("colorchord binding demo", [1400, 480])
         .exit_on_esc(true)
         .build()
         .unwrap();
     window.next();
-    let (tx, rx) = channel();
-    let mut r = Ringbuffer::new(tx);
-    let host = cpal::default_host();
-    let mut notefinder = rustchord::Notefinder::new(48000);
-    let device = host
-        .default_input_device()
-        .expect("Failed to get default input device");
-    println!("{:?}", device.name());
-    let config = &cpal::StreamConfig {
-        channels: 1,
-        buffer_size: cpal::BufferSize::Fixed(1024),
-        sample_rate: cpal::SampleRate(48_000),
-    };
-    let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
-    };
 
-    let stream = device.build_input_stream(
-        config,
-        move |data, inp: &cpal::InputCallbackInfo| r.audio_callback(data, inp),
-        err_fn,
-    );
+    let width = window.size().width / 24.;
+    let notew = window.size().width / 12.;
+    let middle = window.size().height / 2.;
+    let bottom = window.size().height;
 
-    while let Ok(v) = rx.recv() {
-        notefinder.run(&v);
-        let notes = notefinder.get_notes();
-        let res = notefinder.get_folded();
-
-        let width = window.size().width / 24.;
-        let notew = window.size().width / 12.;
-        let middle = window.size().height / 2.;
-        let bottom = window.size().height;
-
-        if let Some(event) = window.next() {
-            window.draw_2d(&event, |context, graphics, _device| {
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _device| {
+            while let Ok(v) = rx.try_recv() {
                 clear([1.0; 4], graphics);
 
                 //Frequency bins
-                for (i, n) in res.into_iter().enumerate() {
+                for (i, n) in v.folded.into_iter().enumerate() {
                     let c = cc_to_rgb((i as f32 + 0.5) / 24., 1.0, 1.0);
 
                     rectangle(
                         [c[0], c[1], c[2], 1.0],
-                        [width * i as f64, bottom, width, -(*n as f64 * 800.)],
+                        [width * i as f64, bottom, width, -(n as f64 * 800.)],
                         context.transform,
                         graphics,
                     );
                 }
 
-                for (i, n) in notes.into_iter().enumerate() {
+                for (i, n) in v.notes.into_iter().enumerate() {
                     if !n.active {
                         continue;
                     }
@@ -82,11 +62,44 @@ fn main() {
                         graphics,
                     )
                 }
-            });
-        }
+            }
+        });
     }
-    std::thread::sleep(std::time::Duration::from_secs(120));
-    drop(stream);
+    //}
+}
+
+fn audioprocess(c: Sender<NoteResult>) {
+    let (tx, rx) = channel();
+    let mut r = Ringbuffer::new(tx);
+    let host = cpal::default_host();
+    let mut notefinder = rustchord::Notefinder::new(48000);
+    let device = host
+        .default_input_device()
+        .expect("Failed to get default input device");
+    println!("{:?}", device.name());
+    let config = &cpal::StreamConfig {
+        channels: 1,
+        buffer_size: cpal::BufferSize::Fixed(512),
+        sample_rate: cpal::SampleRate(48_000),
+    };
+    let err_fn = move |err| {
+        eprintln!("an error occurred on stream: {}", err);
+    };
+    let stream = device.build_input_stream(
+        config,
+        move |data, inp: &cpal::InputCallbackInfo| r.audio_callback(data, inp),
+        err_fn,
+    );
+    while let Ok(v) = rx.recv() {
+        notefinder.run(&v);
+
+        let m = NoteResult {
+            notes: notefinder.get_notes(),
+            folded: notefinder.get_folded().to_owned(),
+        };
+
+        c.send(m).expect("LUL");
+    }
 }
 
 const BUFFER_SIZE: usize = 8096;
